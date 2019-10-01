0c9fd1c1b1672ead06cab1e4bce4f7a8b1c3c7b3
walfrido-oliveira <walfrido_15@hotmail.com>
2019-09-30 23:29:13 -0300

    changed scrypt from footer in the page form usage of Jquery

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{asset('js/jquery.min.js') }}"></script>
+    <script src="{{asset('js/script.js') }}"></script>
