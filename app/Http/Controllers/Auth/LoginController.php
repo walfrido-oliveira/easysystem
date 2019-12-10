5a60ca7597a9a5616dc4657596450a480706200d
walfrido-oliveira <walfrido_15@hotmail.com>
2019-12-10 08:30:46 -0300

    adding active condition in attempt login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use \Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,6 +36,19 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function credentials(Request $request)
+    {
+        $conditions = $request->only($this->username(), 'password');
+        $conditions['active'] = 1;
+        return $conditions;
     }
 
 
