eb0eca55e728e9cdab7eb7845643c78fd207b03a
walfrido-oliveira <walfrido_15@hotmail.com>
2020-03-04 10:18:31 -0300

    adding modal-lg class for 800px width

--- resources/views/modals/clientmodal.blade.php
+++ resources/views/modals/clientmodal.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="searchClient" tabindex="-1" role="dialog" aria-labelledby="searchClientLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="searchClientLabel">Buscar Cliente</h5>
